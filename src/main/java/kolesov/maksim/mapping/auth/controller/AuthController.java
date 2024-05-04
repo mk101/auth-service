591c08e5c3b0ec511d4149702887aecdda216354
Maxim Kolesov <pro100maks222@gmail.com>
2024-05-04 11:24:04 +0300

    Add swagger tag

--- src/main/java/kolesov/maksim/mapping/auth/controller/AuthController.java
+++ src/main/java/kolesov/maksim/mapping/auth/controller/AuthController.java
@@ -1,5 +1,7 @@
 package kolesov.maksim.mapping.auth.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import jakarta.validation.Valid;
 import kolesov.maksim.mapping.auth.dto.CredentialsDto;
 import kolesov.maksim.mapping.auth.dto.ResponseDto;
@@ -13,15 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public interface AuthController {
 
     @PostMapping("/login")
+    @Tags(value = @Tag(name = "Auth"))
     ResponseDto<TokenDto> login(@RequestBody @Valid CredentialsDto credentials);
 
     @PostMapping("/register")
+    @Tags(value = @Tag(name = "Auth"))
     ResponseDto<String> register(@RequestBody @Valid UserDto userDto);
 
     @PostMapping("/refresh")
+    @Tags(value = @Tag(name = "Auth"))
     ResponseDto<TokenDto> refresh(@RequestBody @Valid TokenDto tokenDto);
 
     @PostMapping("/logout")
+    @Tags(value = @Tag(name = "Auth"))
     ResponseDto<Void> logout(@RequestBody @Valid TokenDto tokenDto);
 
 }
--- src/main/java/kolesov/maksim/mapping/auth/controller/KeyController.java
+++ src/main/java/kolesov/maksim/mapping/auth/controller/KeyController.java
@@ -1,5 +1,7 @@
 package kolesov.maksim.mapping.auth.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/auth")
 public interface KeyController {
 
+    @Tags(value = @Tag(name = "Auth"))
     @GetMapping(value = "/public", produces = "application/octet-stream")
     ByteArrayResource getPublicKey();
 
