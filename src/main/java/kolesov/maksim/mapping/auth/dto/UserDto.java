a876cc9eb97704e47a69b7551eb37501c7ce4c04
Maxim Kolesov <pro100maks222@gmail.com>
2024-05-09 15:39:12 +0300

    update password regex

--- src/main/java/kolesov/maksim/mapping/auth/dto/UserDto.java
+++ src/main/java/kolesov/maksim/mapping/auth/dto/UserDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -35,6 +36,7 @@ public class UserDto {
     private String login;
 
     @NotEmpty
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$")
     private String password;
 
     private List<UserRoleDto> roles;
